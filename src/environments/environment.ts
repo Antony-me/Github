af2afe3e848b5f2c821ff711887bdbfae88fb68f
Antony Simiyu <antonymunyasia993@gmail.com>
2020-10-06 21:49:04 +0300

    fix bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  myApi:'33b3ec335783e556794cb6672dceee70ef8b59e3',
 };
 
 /*
